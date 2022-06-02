5244b5d800fc2fb535167e313c4d916e12a06287
Kewal Shah <96408479+kewalDShah@users.noreply.github.com>
2022-06-02 07:32:40 +0000

    Comment Changed

--- main.py
+++ main.py
@@ -1,61 +1,52 @@
-#Learn about Defining List
+#LEARN ABOUT LIST
 #Create a list of all your friends
 friends=["Joy","Happy","Kelly"]
 
-#print list of friends using variable
+#print the list of your friends 
 print(friends)
 
-#Learn to print specific value from a list
 #print specific value
 print(friends[0])
+print(friends[2])
 
-#ask one more friend's name to add to the list
+#Take the input of one more friend's name and add it to the list.
 oneMoreFriend=input("Enter one friend name which you want to add to the list: ")
 
-#Add the name of the newly asked friend to the list.
 friends.append(oneMoreFriend)
 
-#print list of friends using variable
+
+#print list of your friends
 print(friends)
 
-#Learn about if-conditions
-#Add a condition to check if the name of your best friend is in the list
+#LEARN ABOUT CONDITIONS
+#Add a condition to check if your best friend's name is present in the list or not and print a message accordingly
+
 if("Joy" in friends):
-  
-  #Print a message to inform the user that the best friend's name is in the list of friends
   print("Your name is there in the list")
-  
-#Else condtion if frined's name not in list
 else:
   print("Your name not in list")
 
 
 
-#Learn about operators
-#______________What is Operators (==, <,>)
+#LEARN ABOUT OPERATORS(==, <,>) 
   
-#Initialize a variable and assign a number of your choice from 1 to 10
+#Initialize a variable and assign a jackpot number of your choice from 1 to 10
 number=6
 
-#ask for an input from the user for a number
+#Ask the user to guess a number and store it in a variable
 inpNumber=input("Enter a number to check you guess")
 
-#convert the input of number to an integer
+#convert the guessed number to an integer
 inpNumber=int(inpNumber)
 
-#Add a condition to check if the guessed number is greater than the number in the game
-if(inpNumber>number):
+#Add an if condition to check if the guessed number is equal to the jackpot number and print a message accordingly
+if(inpNumber==number):
+  print("You have done it. You got the exact number")
 
-  #print the message that user guessed too high number
+#Add a elif condition to check if the guessed number is greater than the jackpot number and print a message accordingly
+elif(inpNumber>number):
   print("You have guessed too high number")
 
-#Add an elif condition to check if the guessed number is less than the number in the game.
+#Add an elif condition to check if the guessed number is less than the jackpot number and print a message accordingly
 elif(inpNumber<number):
-
-  #print the message that user guessed too small number
   print("You have guessed too small number")
-
-#Add an elif condition to check if the guessed number is equal to the number in the game
-elif(inpNumber==number):
-  #print the message that guessed numnber is correct
-  print("You have done it. You got the exact number")
